64936e0120ea4181591a1ef9d3dbb9bc17e027c6
Трофимов Алексей <a.trofimov@domain.local>
2025-10-20 12:47:46 +0300

    add sandbox

---
+++ src/sandbox.js
@@ -0,0 +1,5 @@
+import { multipliedByN } from "./task1.js";
+import { calculateSum } from "./task2.js";
+import { reverseArray } from "./task3.js";
+import { hasCorrectElement } from "./task4.js";
+
