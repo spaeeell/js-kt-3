7755f30ab4ee7895f013275e7ab3b66104ffc869
Prikhodko <spaelbro@gmail.com>
2025-10-20 14:12:40 +0300

    completed task 1

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,3 +3,5 @@ import { calculateSum } from "./task2.js";
 import { reverseArray } from "./task3.js";
 import { hasCorrectElement } from "./task4.js";
 
+const x = [1,2,3]
+console.log(multipliedByN(x, 5));
--- src/task1.js
+++ src/task1.js
@@ -8,6 +8,10 @@
  * перемножением всех элементов исходного массива на число
  */
 export function multipliedByN(initialArray, multiplier) {
+    const arr = [];
 
-  return initialArray;
+    for(let i = 0; i < initialArray.length; i++)
+        arr.push(initialArray[i] * multiplier)
+
+    return arr;
 }
